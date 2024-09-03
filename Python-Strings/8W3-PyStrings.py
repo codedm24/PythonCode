9a4ad61d6a669869b61f39b9a8f675bdaaf13606
codedm24 <mithun.adroit@gmail.com>
2024-09-03 18:21:15 +0530

    Create 8W3-PyStrings.py

---
+++ Python-Strings/8W3-PyStrings.py
@@ -0,0 +1,30 @@
+print("Hello")
+print('Hello')
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a= "Hello"
+print(a)
+
+a = """Loren ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a ="Hello WOrld!"
+print(a[1])
+
+for x in "banana":
+    print(x)
+
+print("Length: ",len(a))
+
+txt= "The best things in life are free!"
+print("free" in txt)
+print("expensive" in txt)
+print("expensive" not in txt)
+
+if("expensive" not in txt):
+    print("No, 'expensive' is not present.")

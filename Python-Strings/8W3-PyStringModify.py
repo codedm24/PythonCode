8f8d2e9acb610ac4cd350261b7034ece93ef4aab
codedm24 <mithun.adroit@gmail.com>
2024-09-03 18:44:46 +0530

    update file

---
+++ Python-Strings/8W3-PyStringModify.py
@@ -0,0 +1,20 @@
+a = "Hello World!"
+print(a.upper())
+
+print(a.lower())
+
+a = " Hello World! "
+print(a)
+#remove whitespace
+print(a.strip())
+
+#replace string with another string
+print(a.replace("H","J"))
+
+a = "Hello, World!"
+#split string
+
+print(a.split(","))
+
+x, y = a.split(",")
+print(x, "....",y)

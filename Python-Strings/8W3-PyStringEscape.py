f8ed541e2b54e5e1c5bb989bb7bdbb6248852b34
codedm24 <mithun.adroit@gmail.com>
2024-09-04 17:46:50 +0530

    new file

---
+++ Python-Strings/8W3-PyStringEscape.py
@@ -0,0 +1,29 @@
+#error
+# txt = "We are the so-called "Vikings" from the north"
+#double-qupte escape
+txt = "We are the so-called \"Vikings\" from the north"
+print(txt)
+#single quote escape
+txt = 'It\'s alright'
+print(txt)
+#backslash
+txt = "This will insert one \\ backslash"
+print(txt)
+#new line
+txt= "Hello\nWorld!"
+print(txt)
+#carriage return
+txt1 = "abc\r123!"
+print(txt1)
+#tab
+txt1 = "Hello\tWorld!"
+print(txt1)
+#backspace
+txt1 = "Hello \bWorld!"
+print(txt1)
+#octal value
+txt1 = "\110\145\154\154\157"
+print(txt1)
+#hex value
+txt1 = "\x48\x65\x6c\x6c\x6f"
+print(txt1)

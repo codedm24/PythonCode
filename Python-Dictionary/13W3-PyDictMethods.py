589935817ab25865865d2cb254318f9211e46751
codedm24 <mithun.adroit@gmail.com>
2024-09-12 13:55:42 +0530

    new dictionary file added

---
+++ Python-Dictionary/13W3-PyDictMethods.py
@@ -0,0 +1,87 @@
+car = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year"  : 1964
+}
+
+car.clear()
+print(car)
+
+car = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year"  : 1964
+}
+
+x = car.copy()
+print(x)
+
+x = ("key1", "key2", "key3")
+y = 0
+thisdict = dict.fromkeys(x,y)
+print(thisdict)
+
+x = ("key1","key2","key3")
+thisdict = dict.fromkeys(x)
+print(thisdict)
+
+car = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+x = car.get("model")
+print(x)
+
+x = car.get("price",15000)
+print(x)
+
+x = car.items()
+print(x)
+
+car["year"] = 2018
+print(car)
+
+x = car.keys()
+print(x)
+
+car["color"] = "white"
+print(car)
+
+car = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+
+car.pop("model")
+print(car)
+
+car = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+car.popitem()
+print(car)
+
+car = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+
+x = car.setdefault("model", "Bronco")
+print(x)
+
+x = car.setdefault("color","white")
+print(x)
+
+car = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+
+car.update({"color":"white"})
+print(car)
---
+++ Python-Dictionary/13W3-PyNestedDict.py
@@ -0,0 +1,43 @@
+myFamily = {
+    "child1" : {
+        "name":"Emil",
+        "year":2004
+    },
+    "child2" :{
+        "name":"Tobias",
+        "year":2007
+    },
+    "child3": {
+        "name":"Linus",
+        "year":2011
+    }
+}
+
+print(myFamily)
+
+child1 = {
+        "name":"Emil",
+        "year":2004
+    }
+child2 = {
+        "name":"Tobias",
+        "year":2007
+    }
+child3 = {
+        "name":"Linus",
+        "year":2011
+    }
+
+myFamily = {
+    "child1" : child1,
+    "child2" : child2,
+    "child3" : child3
+}
+
+print(myFamily["child2"]["name"])
+
+for x,obj in myFamily.items():
+    print(x)
+
+    for y in obj:
+        print(y + ':', obj[y])
